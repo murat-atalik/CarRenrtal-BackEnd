bebebf0e946d0a3e202952bfcd09150c85046660
Murat ATALIK <62357472+murat-atalik@users.noreply.github.com>
2021-03-21 22:28:55 +0300

    Dto's added

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+   public class User:IEntity
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Entities/DTOs/RentalDetailDto.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class RentalDetailDto:IDto
+    {
+        public int RentalId { get; set; }
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+        public string CompanyName { get; set; }
+        public string EmailAddress { get; set; }
+        public string CarName { get; set; }
+        public string CarBrand { get; set; }
+        public string CarColor { get; set; }
+        public decimal DailyPrice { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
