d687106a235b908d8996b771b8ebb192a15d4397
Murat ATALIK <62357472+murat-atalik@users.noreply.github.com>
2021-03-21 15:44:39 +0300

    Fix

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -16,10 +16,12 @@ namespace ConsoleUI
             // BrandOperations();
 
             CarManager carManager = new CarManager(new EfCarDal());
-            foreach (var car in carManager.GetCarDetails())
+            foreach (var car in carManager.GetCarDetails().Data)
             {
-                Console.WriteLine(" Car ID:"+car.CarId+" Name: "+car.CarName+" Model " + car.BrandName+" Renk: " + car.ColorName+" Günlük kira: "+car.DailyPrice);
+                Console.WriteLine(" Car ID:" + car.CarId + " Name: " + car.CarName + " Model " + car.BrandName + " Renk: " + car.ColorName + " Günlük kira: " + car.DailyPrice);
             }
+
+
         }
 
         private static void BrandOperations()
@@ -42,23 +44,23 @@ namespace ConsoleUI
             };
             Console.WriteLine("---- Add brand ----");
             brandManager.Add(brand1);
-            foreach (var brand in brandManager.GetAll())
+            foreach (var brand in brandManager.GetAll().Data)
             {
                 Console.WriteLine(brand.BrandId + " " + brand.BrandName);
             }
             Console.WriteLine("---- Delete brand ----");
             brandManager.Delete(brand2);
-            foreach (var brand in brandManager.GetAll())
+            foreach (var brand in brandManager.GetAll().Data)
             {
                 Console.WriteLine(brand.BrandId + " " + brand.BrandName);
             }
             Console.WriteLine("---- Update brand ----");
             brandManager.Update(brand3);
-            foreach (var brand in brandManager.GetAll())
+            foreach (var brand in brandManager.GetAll().Data)
             {
                 Console.WriteLine(brand.BrandId + " " + brand.BrandName);
             }
-            Brand brandId = brandManager.GetAllById(7);
+            Brand brandId = brandManager.GetById(7).Data;
             Console.WriteLine(brandId.BrandId + " " + brandId.BrandName);
         }
 
@@ -81,23 +83,23 @@ namespace ConsoleUI
             };
             Console.WriteLine("-----Add Operation-----");
             colorManager.Add(color1);
-            foreach (var color in colorManager.GetAll())
+            foreach (var color in colorManager.GetAll().Data)
             {
                 Console.WriteLine(color.ColorId + " " + color.ColorName);
             }
             Console.WriteLine("-----Delete Operation-----");
             //colorManager.Delete(color2);
-            foreach (var color in colorManager.GetAll())
+            foreach (var color in colorManager.GetAll().Data)
             {
                 Console.WriteLine(color.ColorId + " " + color.ColorName);
             }
             Console.WriteLine("-----Update Operation-----");
             colorManager.Update(color3);
-            foreach (var color in colorManager.GetAll())
+            foreach (var color in colorManager.GetAll().Data)
             {
                 Console.WriteLine(color.ColorId + " " + color.ColorName);
             }
-            Console.WriteLine(colorManager.GetAllById(1).ColorId + " " + colorManager.GetAllById(1).ColorName);
+            Console.WriteLine(colorManager.GetById(1).Data.ColorId + " " + colorManager.GetById(1).Data.ColorName);
         }
 
         private static void CarOperations()
@@ -118,11 +120,11 @@ namespace ConsoleUI
             carManager.Add(car1);                                                                       //Worked 101.Eleman eklendi
             carManager.Update(car1);                                                                     //Worked 99 ve 100. elemanlar güncellendi
             carManager.Delete(car1);                                                                      //Worked 98. eleman silindi
-            foreach (var car in carManager.GetAll())                                                      //Worked tüm elemanlar çağırldı
+            foreach (var car in carManager.GetAll().Data)                                                      //Worked tüm elemanlar çağırldı
             {
                 Console.WriteLine(car.CarId + " " + car.Description + " " + car.DailyPrice);
             }
-            Console.WriteLine(carManager.GetAllById(101).CarId + carManager.GetAllById(101).Description);  //Worked GetById
+            Console.WriteLine(carManager.GetById(101).Data.CarId + carManager.GetById(101).Data.Description);  //Worked GetById
         }
     }
 }
