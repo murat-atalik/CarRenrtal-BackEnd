6da2a253155136fb8d0272dd71f42c587f66cf6b
Murat ATALIK <62357472+murat-atalik@users.noreply.github.com>
2021-03-21 15:44:27 +0300

    Core Layer Results added

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool succes,string message):this(succes)
+
+        {
+            message = Message;
+        }
+        public Result(bool succes)
+        {
+            succes = Success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
