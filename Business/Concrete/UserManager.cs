5ab32c6453c055bbd59b08d9a68ff5badfc44ad9
Murat ATALIK <62357472+murat-atalik@users.noreply.github.com>
2021-03-25 14:51:52 +0300

    fluent validation fix

--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
